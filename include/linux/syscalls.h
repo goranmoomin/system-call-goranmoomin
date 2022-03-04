8f6ec43365df70fcedb1cc204ddd282c67e117d9
Sungbin Jo <goranmoomin@daum.net>
2022-03-04 15:58:47 +0900

    Add simple hello syscall

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -690,6 +690,9 @@ asmlinkage long sys_getegid(void);
 asmlinkage long sys_gettid(void);
 asmlinkage long sys_sysinfo(struct sysinfo __user *info);
 
+/* kernel/hello.c */
+asmlinkage long sys_hello(void);
+
 /* ipc/mqueue.c */
 asmlinkage long sys_mq_open(const char __user *name, int oflag, umode_t mode, struct mq_attr __user *attr);
 asmlinkage long sys_mq_unlink(const char __user *name);
---
+++ kernel/hello.c
@@ -0,0 +1,10 @@
+#include <linux/syscalls.h>
+#include <linux/printk.h>
+
+SYSCALL_DEFINE0(hello)
+{
+	char *my_name = "Sungbin Jo";
+	static int num_of_calls = 0;
+	printk("Hello, this is syscall by %s! (%d)\n", my_name, num_of_calls++);
+	return 0;
+}
